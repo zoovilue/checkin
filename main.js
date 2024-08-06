e6b995577945a18a7fe39c3d104cfb2977982004
zoovilue <19821237538@163.com>
2024-08-06 12:46:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,7 @@ const glados = async () => {
   if (!cookie) return
   try {
     const headers = {
-      'cookie': 'b-user-id=27d14bee-3a9c-bd2f-c3fb-ce727347ae35;
-    _gid=GA1.2.1065512435.1722916786;
-    _ga=GA1.1.230021712.1722916786; koa:sess=eyJ1c2VySWQiOjMyNTEwMSwiX2V4cGlyZSI6MTc0ODgzNjgzMjk3NSwiX21heEFnZSI6MjU5MjAwMDAwMDB9;
-    koa:sess.sig=3O16EOLthVMF0EoGlezvx2YeDx0;
-    _ga_CZFVKMNT9J=GS1.1.1722916786.1.1.1722916833.0.0.0',
+      'cookie':cookie,
       'referer': 'https://glados.rocks/console/checkin',
       'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
     }
